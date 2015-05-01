f2639230181b8cdf68cf0cc6233b5ed2fb1c1ec8
deqing.qdq <deqing.qdq@alibaba-inc.com>
2015-05-02 00:35:16 +0800

    add logout feature

---
+++ js/logout.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', 'http://login.taobao.com/member/logout.jhtml');
+  xhr.send(null);
+});
+
+console.log('logout.js')
